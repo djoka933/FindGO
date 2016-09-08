a21a3512c9bbc71758d46054c25900add6e8d988
djoka933 <mare93ja@gmail.com>
2016-09-08 15:41:22 +0200

    Dodate funkcionalnosti implementiranih funkcija. Update-ovati layout.

---
+++ app/src/main/java/com/example/mare/findgo/AppActivity.java
@@ -0,0 +1,22 @@
+package com.example.mare.findgo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mare/findgo/FindGoApp.java
@@ -0,0 +1,12 @@
+package com.example.mare.findgo;
+
+import android.app.Application;
+
+/**
+ */
+public class FindGoApp extends Application {
+    private static final String TAG = "FindGoApp";
+
+    public Integer UserId;
+    public String Username;
+}
